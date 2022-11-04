65d3582e4e4f808f4eae28d0b04c792958487970
Faeze <faezeghorbanpour96@gmail.com>
2022-11-05 01:13:12 +0330

    projects style changed

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,11 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+function toggle(id) {
+   var el = document.getElementById(id);
+   if(el.style.display == 'block')
+       el.style.display = 'none';
+   else
+     el.style.display = 'block';
+}
